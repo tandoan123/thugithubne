a75296e200bc2cda503c9bcee0ff08c9a3534fe1
tandoan123 <tandnps17985@fpt.edu.vn>
2021-11-03 20:41:32 +0700

    nhật tân vừa thêm vào hàng 14,15 ở MainActivity nha

--- app/src/main/java/com/tan/abcde/MainActivity.java
+++ app/src/main/java/com/tan/abcde/MainActivity.java
@@ -3,12 +3,15 @@ package com.tan.abcde;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // vừa code vừa ghi chú sẽ giúp code dễ hơn cho tất cả mọi người
+        Toast.makeText(this, "hello how are you ?", Toast.LENGTH_SHORT).show();
     }
 }
